fd5f506278431efda414ef5f3d370505d6b7f72d
uuty778 <wodimo3673@missfuli.com>
2026-09-07 18:10:52 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ api_hash = "735a5e369c70f328eab9ad3c52c3b5cf"
 
 client = TelegramClient(StringSession("1BVtsOJwBu49t0kwYRsMp3tbGg1Ulu0Vyxox-C5bMS0vkEZOZmhwI9YSlHsXWgpyHwlLECg36zd_yiITwUkMjaDsI7wR6NtyfIRVQLy5KT9HqTMfUXkmKmcGiaDpdIwR8i79oCIi-U4K0ynrTJPsuZOx9xOYXrFBvdKceeD2FYStw1q3CkQouCc2jsPVntT2r_DvbsFARjCrcHlKJaqMI5G02i9JtDIpVtqEATjw0BvfX3c0ouDja2eYkHFCflqi04qg65ZRbtDTX4lOgQ-ZGrX4RCGY7LLNwCWRpqWBpjpvTGDi-1t5YWDz1X9lNpbucYCj7_bfUqsFvRK3whir1l7-Zs9_xi1A="), api_id, api_hash)
 
-TARGET = "@ttjr888"
+TARGET = "@dd28"
 ADMIN = None  # 你自己的TG用户ID，后面可以填
 
 # 状态存储
